6d6cad31213fdfd6dd4b0232cbbc07092169a74b
Matthew Beale <matt.beale@madhatted.com>
2015-11-05 19:52:27 -0500

    Nitpick and improve the Ember version

--- examples/ember/app.js
+++ examples/ember/app.js
@@ -1,43 +1,55 @@
-threadit = Ember.Application.create();
-//Activate no # mode.  
-threadit.Router.reopen({location: "auto"});
+var threadit = Ember.Application.create();
 
-//Index route is implied.  
+//Use the HTML5 History API for URLs
+threadit.Router.reopen({location: "history"});
+
+//Index route is implied.
 threadit.Router.map(function() {
 	this.route("thread", {path: "/thread/:id"});
 });
 
-//as are controllers.
+function ajax(method, url, data) {
+	data = data || {};
+	return new Ember.RSVP.Promise(function(resolve, reject) {
+		$.ajax(url, {
+			data : data,
+			method : method,
+			failure : Ember.run.bind(null, reject),
+			success : Ember.run.bind(null, resolve)
+		});
+	});
+}
+
+threadit.ApiService = Ember.Service.extend({
+	fetchThreads : function() {
+		return ajax('GET', T.apiUrl + "/threads/");
+	},
+	createThread : function(newText) {
+		return ajax('POST', T.apiUrl + "/threads/create", {
+			text: newText
+		});
+	},
+	fetchComments : function(id) {
+		return $.getJSON(T.apiUrl + "/comments/" + id);
+	},
+	createComment : function(data) {
+		return ajax('POST', T.apiUrl + "/comments/create", data);
+	}
+});
+
+threadit.TrimHelper = Ember.Helper.helper(function(params) {
+	return T.trimTitle(params[0]);
+});
 
 threadit.IndexRoute = Ember.Route.extend({
+	api : Ember.inject.service(),
 	model : function() {
-		return $.getJSON(T.apiUrl + "/threads/")
+		return this.get('api').fetchThreads()
 			.then(function(data) {
-				var str;
-				//Rant about being nuable to define helpers without ember-cli is referring
-				//to this.
-				for(var i = 0; i < data.data.length; i++) {
-					data.data[i].text = T.trimTitle(data.data[i].text);
-				}
 				return {
 					threads: data.data
 				};
 			});
-	},
-	actions : {
-		newThread : function() {
-			var model = this.currentModel;
-			$.post(T.apiUrl + "/threads/create",{
-				text: model.newText
-			})
-			.then(function(response) {
-				//Ember.set tracks what needs to be redrawn.  
-				Ember.set(model, "newText", "");
-				var newList = model.threads.slice();
-				newList.push(response.data);
-				Ember.set(model, "threads", newList);
-			});
-		}
 	},
 	afterModel : function(model) {
 		//It's unfair to criticize Ember for this, no framework handles the document title well.
@@ -45,32 +57,53 @@ threadit.IndexRoute = Ember.Route.extend({
 	}
 });
 
-threadit.ThreadRoute = Ember.Route.extend({
-	model : function(params) {
-		return $.getJSON(T.apiUrl + "/comments/" + params.id)
-			.then(T.transformResponse);
-	},
+threadit.IndexController = Ember.Controller.extend({
+	api : Ember.inject.service(),
 	actions : {
-		showReply : function(node) { 
-			Ember.set(node, "replying", true);
-		},
-		newComment : function(node) {
-			$.post(T.apiUrl + "/comments/create", {
-				text : node.newText,
-				parent : node.id
-			})
+		newThread : function() {
+			var controller = this;
+			var newText = this.get('newText');
+			this.get('api').createThread(newText)
 			.then(function(response) {
-				//Reset the form state.
-				Ember.set(node, "replying", false);
-				Ember.set(node, "newText", "");
-				//Create a new object with the new comment.
-				var newChildren = node.children.slice();
-				newChildren.push(response.data);
-				Ember.set(node, "children", newChildren);
+				controller.set('newText', '');
+				controller.get('model.threads').pushObject(response.data);
 			});
 		}
+	},
+});
+
+threadit.ThreadRoute = Ember.Route.extend({
+	api : Ember.inject.service(),
+	model : function(params) {
+		return this.get('api').fetchComments(params.id)
+			.then(T.transformResponse);
 	},
 	afterModel : function(model) {
 		document.title = "ThreaditJS: Ember | " + T.trimTitle(model.root.text, 80);
 	}
 });
+
+threadit.ThreadItemComponent = Ember.Component.extend({
+	api : Ember.inject.service(),
+	actions : {
+		showReply : function() {
+			this.set('isReplying', true);
+		},
+		newComment : function(node) {
+			var component = this;
+			this.get('api').createComment({
+				text : component.get('newText'),
+				parent : node.id
+			})
+			.then(function(response) {
+				//Reset the form state.
+				component.set('isReplying', false);
+				component.set('newText', '');
+				//Create a new object with the new comment.
+				component.get('model.children').pushObject(response.data);
+			});
+		}
+	},
+});
+
+# vim: set noexpandtab:
