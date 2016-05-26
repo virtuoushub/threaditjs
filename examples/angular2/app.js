8af6c1b862cbb083da77111cc63fb0d061f0de5a
Travis Mitchell <tcmitche@users.noreply.github.com>
2016-05-25 20:42:36 -0700

    More angular 2 working, a thread list even!

--- examples/angular2/app.js
+++ examples/angular2/app.js
@@ -1,3 +1,21 @@
+var Thread = ng.core.Class({
+	constructor: function(id, text, children, commentCount) {
+		this.id = id
+		this.text = text
+		// this.children = children
+		this.commentCount = commentCount
+	}
+})
+
+var ThreadList = [
+	new Thread(1, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod', [], 1234),
+	new Thread(2, 'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,', [], 2345),
+	new Thread(3, 'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo', [], 3456),
+	new Thread(4, 'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse', [], 4567),
+	new Thread(5, 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non', [], 5678),
+	new Thread(6, 'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', [], 6789)
+]
+
 var AppComponent = ng.core.Component({selector: "my-app"})
 .View({
 	directives: [ng.common.CORE_DIRECTIVES],
@@ -5,18 +23,16 @@ var AppComponent = ng.core.Component({selector: "my-app"})
 })
 .Class({
 	constructor: function() {
-		this.update()
-	},
-	update: function() {
-		var self = this
-
+		this.threads = ThreadList
+		this.trimTitle = T.trimTitle;
 	},
 	ngAfterViewChecked: function() {
 
-	},
+	}
 })
 
 document.addEventListener("DOMContentLoaded", function() {
 	ng.core.enableProdMode()
 	ng.platform.browser.bootstrap(AppComponent)
 })
+
